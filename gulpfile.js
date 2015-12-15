2987e33a412880d6bc8cec814a2888adf06fad67
bushuai <ibushuai@gmail.com>
2015-12-16 01:01:36 +0800

    optimized ☘

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    concatcss = require('gulp-concat-css'),
+    cssmin = require('gulp-cssmin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    notify = require('gulp-notify'),
+    browserSync = require('browser-sync').create();
+
+/*
+IMAGE
+ */
+gulp.task('images', function() {
+    return gulp.src('./images/*')
+        .pipe(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        }).on('error', function(err) {
+            console.log(err.message)
+        }))
+        .pipe(gulp.dest('./assets/images/'))
+        .pipe(notify({
+            message: 'Images task complete'
+        }));
+});
+
+/*
+Javascripts
+ */
+gulp.task('javascripts', function() {
+    return gulp.src('./javascripts/**/*.js')
+        .pipe(uglify().on('error', function(err) {
+            console.log(err.message);
+        }))
+        .pipe(gulp.dest('./assets/javascripts/'))
+        .pipe(notify({
+            message: 'Javascripts task complete'
+        }));
+});
+
+/*
+Styles
+ */
+gulp.task('styles', function() {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', function(err) {
+            console.log(err.message);
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ff', 'Chrome', 'ie'],
+            cascade: true,
+            remove: true
+        }))
+        .pipe(cssmin())
+        .pipe(gulp.dest('./assets/css/'))
+        .pipe(notify({
+            message: 'Styles task complete'
+        }));
+});
+
+/*
+WATCH
+ */
+gulp.task('watch', function() {
+    gulp.watch('./sass/**/*.scss', ['styles']);
+    gulp.watch('./javascripts/**/*.js', ['javascripts']);
+    gulp.watch('./images/*', ['images']);
+});
+
+gulp.task('default', ['watch']);
